90a8ff6fc86cb0f484398d29ae96137e57be86f7
Ori Markowitz <ori.markowitz@gmail.com>
2020-12-09 23:03:28 -0500

    commit tree 662a481ff5c21701c91501bbef6926bca7dc869d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
+  end
 end
